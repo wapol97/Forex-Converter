8e7baef9330348128de6de2553730920aeb42c44
Wade <wadeapollard@gmail.com>
2023-04-06 16:49:45 -0400

    #

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from forex_python.converter import CurrencyRates
+import requests
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "1234567890"
 debug = DebugToolbarExtension(app)
+
+currency = CurrencyRates()
+
+@app.route("/currency-exchange")
+def currency_exchange():
+    from_curr = request.form['from_curr']
+    to_curr = request.form['to_curr']
+    amount = request.form['amount']
+
+    result = currency.convert(from_curr, to_curr, amount)
+    
+    return render_template('base.html', result)
+
+    
