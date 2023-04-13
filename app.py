40bbb6677284abbb1e93eb119b7801c2b198f497
Wade <wadeapollard@gmail.com>
2023-04-13 16:18:29 -0400

    Second Commmit

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from forex_python.converter import CurrencyRates
-import requests
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "1234567890"
 debug = DebugToolbarExtension(app)
 
-currency = CurrencyRates()
 
-@app.route("/currency-exchange")
+@app.route("/currency-exchange", methods=["GET"])
 def currency_exchange():
-    from_curr = request.form['from_curr']
-    to_curr = request.form['to_curr']
-    amount = request.form['amount']
+    return render_template('base.html')
 
-    result = currency.convert(from_curr, to_curr, amount)
-    
-    return render_template('base.html', result)
+@app.route("/currency-exchange", methods=["POST"])
+def exchange_result():
+    currency = CurrencyRates()
+    from_curr = request.form["from_curr"]
+    to_curr = request.form["to_curr"]
+    amount = request.form["amount"]
 
+    msg = currency.convert(from_curr, to_curr, amount)
     
+    flash(f"Your result is: {msg}", "result")
+
+    return redirect('/currency-exchange')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
